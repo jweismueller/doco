60a4c2187d947df7ab11c401ebe64fe61d6702e2
Jürgen Weismüller <juergen.weismueller@prodyna.com>
2024-03-20 13:52:38 +0100

    fix: delete admin user

---
+++ src/main/java/de/weismueller/doco/controller/ErrorController.java
@@ -0,0 +1,19 @@
+package de.weismueller.doco.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        log.error("Error: {}", status);
+        return "error";
+    }
+}
